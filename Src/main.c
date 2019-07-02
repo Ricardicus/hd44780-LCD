c134dd7699f58b3949969f0d61f5ad5e3b8656b2
Rickard Hallerbäck <rickard_hallerback@hotmail.com>
2019-07-02 22:33:15 +0200

    Some notes

--- Src/main.c
+++ Src/main.c
@@ -1,22 +1,35 @@
-/* USER CODE BEGIN Header */
+/*
+*
+*
+* Picked up an LCD display from my arduino starters kit 
+* I got some years ago. I wanted to make a driver for it
+* just to see what that would be like. 
+* I use an stm32f407vg discovery board and documentation 
+* for the LCD that I got online from:
+* https://www.sparkfun.com/datasheets/LCD/HD44780.pdf 
+* (accessed 2 july, 2019)
+*
+*
+*/
+
+
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+******************************************************************************
+* @file           : main.c
+* @brief          : Main program body
+******************************************************************************
+* @attention
+*
+* <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+* All rights reserved.</center></h2>
+*
+* This software component is licensed by ST under Ultimate Liberty license
+* SLA0044, the "License"; You may not use this file except in compliance with
+* the License. You may obtain a copy of the License at:
+*                             www.st.com/SLA0044
+*
+******************************************************************************
+*/
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
